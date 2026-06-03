88ee49084a4f4bc2d44780c226714543a3fa532a
Alireza-Hamidi2001 <alireza.hamidi.eng@gmail.com>
2026-06-03 20:09:45 +0330

    download again node module packages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -9,17 +8,39 @@ const nextConfig = {
                 port: "",
                 pathname: "/storage/v1/object/public/cabin-images/**",
             },
-            {
-                protocol: "https",
-                hostname: "lh3.googleusercontent.com",
-                port: "",
-                pathname: "/**",
-            },
         ],
     },
-    experimental: {
-        cacheComponents: true,
-    },
+
+    cacheComponents: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
+// const path = require("path");
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     // در Next.js 16، turbopack یک کلید سطح بالاست (نه داخل experimental)
+//     turbopack: {
+//         root: path.join(__dirname),
+//     },
+
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: "https",
+//                 hostname: "dclaevazetcjjkrzczpc.supabase.co",
+//                 port: "",
+//                 pathname: "/storage/v1/object/public/cabin-images/**",
+//             },
+//             {
+//                 protocol: "https",
+//                 hostname: "lh3.googleusercontent.com",
+//                 port: "",
+//                 pathname: "/**",
+//             },
+//         ],
+//     },
+// };
+
+// module.exports = nextConfig;
