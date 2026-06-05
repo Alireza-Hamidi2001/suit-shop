768876e8fed4c5227e1cfbc0fef5639bd75cbfeb
Alireza-Hamidi2001 <alireza.hamidi.eng@gmail.com>
2026-06-05 23:26:32 +0330

    change responsive mobile 3

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import ScrollReveal from "./_components/ScrollReveal";
 export default function Home() {
     return (
         <main className="">
-            <div className="absolute font-lime text-6xl  left-4 bottom-50 sm:left-16 sm:bottom-16 w-[90%] md:w-[60%] lg:w-[50%]">
+            <div className="absolute font-lime text-6xl  left-4 bottom-49 sm:left-16 sm:bottom-16 w-[90%] md:w-[60%] lg:w-[50%]">
                 <ScrollReveal direction="right" className="flex-1 space-y-6">
                     <h1 className="bg-dark text-black dark:text-white">
                         Timeless Elegance, Tailored for You
